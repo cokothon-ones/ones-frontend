94ed6ac0ee3a7563757b8a17ab9275f1d697fdd8
cjeongmin <cjeongmin27@gmail.com>
2023-12-26 23:09:30 +0900

    feat: add home nav bar

--- OnesFrontend/Views/MainView.swift
+++ OnesFrontend/Views/MainView.swift
@@ -8,12 +8,60 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var isSelected: Bool = true
     private let column = [GridItem(.flexible(), spacing: 20),
                           GridItem(.flexible(), spacing: 20)]
 
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             VStack(spacing: 0) {
+                Spacer(minLength: 20)
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        Text("잠김")
+                            .frame(width: 175)
+                            .foregroundColor(self.isSelected ? Color(0x1DAFFF) : Color(0xACACAC))
+                            .onTapGesture {
+                                withAnimation {
+                                    self.isSelected = true
+                                }
+                            }
+
+                        Text("열림")
+                            .frame(width: 175)
+                            .foregroundColor(self.isSelected ? Color(0xACACAC) : Color(0x1DAFFF))
+                            .onTapGesture {
+                                withAnimation {
+                                    self.isSelected = false
+                                }
+                            }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .padding(.top, 10)
+                    .padding(.bottom, 10)
+
+                    ZStack(alignment: .leading) {
+                        Rectangle()
+                            .fill(.clear)
+                            .frame(width: 175 * 2, height: 4)
+
+                        Rectangle()
+                            .fill(RadialGradient(
+                                colors: [
+                                    Color(0xAAD7FF),
+                                    Color(0xD2D7FF)
+                                ],
+                                center: .center,
+                                startRadius: 0,
+                                endRadius: 90
+                            ))
+                            .frame(width: 175, height: 4)
+                            .offset(x: self.isSelected ? 0 : 175)
+                    }
+                }
+                .background(Color.white)
+
                 ScrollView(showsIndicators: false) {
                     LazyVGrid(columns: column, spacing: 5) {
                         ForEach(1 ... 10, id: \.self) { _ in
