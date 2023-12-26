5a8568df6cfdc5f0f3f5e08dc06b54499cee33ea
cjeongmin <cjeongmin27@gmail.com>
2023-12-26 16:31:17 +0900

    feat: init

---
+++ ones-frontend/ones_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ones_frontendApp.swift
+//  ones-frontend
+//
+//  Created by 최정민 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
