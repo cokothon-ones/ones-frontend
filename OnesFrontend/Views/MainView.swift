dee21a237631cc14da80a84c8bb744d23910381c
cjeongmin <cjeongmin27@gmail.com>
2023-12-26 16:56:38 +0900

    feat: add Color Extensions

---
+++ OnesFrontend/Views/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  OnesFrontend
+//
+//  Created by 최정민 on 12/26/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
